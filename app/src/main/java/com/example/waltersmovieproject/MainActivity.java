a45979fce6a1a80605e4ea5b35e327e0092664b3
Xploss <iqbalma.00@gmail.com>
2021-09-27 20:40:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/waltersmovieproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.waltersmovieproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
